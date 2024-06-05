5dafeddf70173246dcb9eeeccc1552e681d44631
developer <flores_ramirez_ricardo@hotmail.com>
2024-06-05 05:19:03 -0600

    refactor: deleted file

--- src/components/main-frame/main-frame.tsx
+++ src/components/main-frame/main-frame.tsx
@@ -3,8 +3,6 @@ import Box from '@mui/material/Box';
 import AppHeader from "../app-header/app-header";
 import PageFrame from "../page-frame/page-frame";
 
-// import './main-frame.css'
-
 
 export default function MainFrame() {
     return <Box sx={{width: 'calc(100vw - 251px)', height: '100%', maxHeight: '100vh', overflow: 'hidden'}}>
